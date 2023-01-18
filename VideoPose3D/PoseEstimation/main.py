0f46157bc07ecbe65ed628958f8adca98af9820f
Albert <wensinlor@gmail.com>
2023-01-18 09:31:13 +0800

    Write an algorithm to handle mutiple npy files
    Signed-off-by: Albert <wensinlor@gmail.com>

---
+++ VideoPose3D/PoseEstimation/main.py
@@ -0,0 +1,133 @@
+import os
+
+from utils.keypoints import Keypoints
+from database.database import Database
+from pprint import pprint
+
+
+JSON_FILENAME = {
+    "LEFT_HAND": "left_hand_dict_json.json",
+    "LEFT_SHOULDER": "left_shoulder_dict_json.json",
+    "LOW_BACK": "low_back_dict_json.json",
+    "RIGHT_HAND": "right_hand_dict_json.json",
+    "RIGHT_SHOULDER": "right_shoulder_dict_json.json"
+}
+
+
+"""
+Specify which person we are going to study
+"""
+while True:
+    INDIVIDUAL = input("Which person are you specifying?: ")
+    if str(INDIVIDUAL).isdigit() == True:
+        break
+    else:
+        print('Please enter an integer')
+        continue
+
+
+"""
+Select an option about reading an existing data or create a new one
+"""
+while True:
+    EXIST = input('Read an existing data? (True/False): ')
+    if EXIST in ['TRUE', 'True', 'true', 'T', 't']:
+        NEW_KEYPOINTS = False
+        break
+    elif EXIST in ['FALSE', 'False', 'false', 'F', 'f']:
+        NEW_KEYPOINTS = True
+        break
+    else:
+        print('Please enter a valid command')
+        continue
+
+# If reading an existing database
+if NEW_KEYPOINTS == False:
+    BODY_PART_LIST = []
+    while True:
+        BODY_PART = input('Enter a body part of interest: ')
+        BODY_PART_LIST.append(BODY_PART)
+        answer = input('Any other body part of interest?: ')
+        if answer in ['Yes', 'yes', 'Y', 'y']:
+            continue
+        elif answer in ['No', 'no', 'N', 'n']:
+            break
+        else:
+            print('Please enter a valid command')
+
+    JSONFILEPATH_DICT = {}
+    for BODY_PART in BODY_PART_LIST:
+        JSONFILEPATH = rf"C:\Users\ASUS\Academic\URECA\VideoPose3D\PoseEstimation\database\json_data\Individual_{INDIVIDUAL}\{JSON_FILENAME[BODY_PART]}"
+        JSONFILEPATH_DICT[BODY_PART] = JSONFILEPATH
+
+    database = Database(jsonPathDict=JSONFILEPATH_DICT, individual=INDIVIDUAL)
+
+# If creating a new database
+else:
+    NPYFILEPATH = r"C:\Users\ASUS\Academic\URECA\VideoPose3D\PoseEstimation\database\npy_data\albert_output.npy"
+    database = Database(npyPath=NPYFILEPATH, individual=INDIVIDUAL)
+
+
+"""
+Specify the directory where the JSON file has to be written to. 
+"""
+PARENT_DIRECTORY = r"C:\Users\ASUS\Academic\URECA\VideoPose3D\PoseEstimation\database\json_data"
+DIRECTORY = f"Individual_{INDIVIDUAL}"
+PATH = os.path.join(PARENT_DIRECTORY, DIRECTORY)
+
+
+def read_from_database(new_keypoints=True):
+    """
+    1. Start the entire pipeline by retrieving the dataset from the database
+    2. If 'new_keypoints' is specified as 'True', writes the new result to the database
+    3. If not, it's just simply reading the existing data
+    """
+    if new_keypoints == True:
+        coordinates = database.read_npy_data()
+        write_to_database(coordinates)
+
+    else:
+        coordinates = database.read_json_data()
+        
+        body_part_dict = {}
+        coordinates_list = []
+        count = 0
+        for coordinate in coordinates.values():
+            for i in coordinate.values():
+                coordinates_list.append(i)
+        
+            body_part_dict[BODY_PART_LIST[count]] = coordinates_list
+            count += 1
+
+        pprint(body_part_dict, indent=4)
+    
+def write_to_database(coordinates):
+    """
+    1. Check if the directory exists
+    2. Write the JSON file into the database
+    """
+    create_directory()
+    keypoints = Keypoints(coordinates, individual=INDIVIDUAL)
+    json_file_dict = {}
+    json_file_dict['low_back_dict_json'] = keypoints.low_back_keypoint()
+    json_file_dict['left_shoulder_dict_json'] = keypoints.left_shoulder_keypoint()
+    json_file_dict['right_shoulder_dict_json'] = keypoints.right_shoulder_keypoint()
+    json_file_dict['left_hand_dict_json'] = keypoints.left_hand_keypoint()
+    json_file_dict['right_hand_dict_json'] = keypoints.right_hand_keypoint()
+
+    for filename, file in json_file_dict.items():
+        database.write_json_data(json_file=file, json_filename=filename)
+
+def create_directory():
+    """
+    Create a new directory if the directory doesn't exist
+    """
+    if os.path.isdir(PATH) == False:
+        os.mkdir(PATH)
+        print(f'Successfully created directory {DIRECTORY}!')  
+
+def main():
+    read_from_database(new_keypoints=NEW_KEYPOINTS)
+
+if __name__ == '__main__':
+    main()
---
+++ VideoPose3D/PoseEstimation/utils/keypoints.py
@@ -0,0 +1,96 @@
+import json
+
+
+#17 Keypoints
+BOTTOM_TORSO = 0
+LEFT_HIP = 1
+LEFT_KNEE = 2
+LEFT_FOOT = 3
+RIGHT_HIP = 4
+RIGHT_KNEE = 5
+RIGHT_FOOT = 6
+CENTER_TORSO = 7
+UPPER_TORS0 = 8 #neck
+NOSE = 9 
+CENTER_HEAD = 10
+RIGHT_SHOULDER = 11
+RIGHT_ELBOW = 12
+RIGHT_HAND = 13
+LEFT_SHOULDER = 14
+LEFT_ELBOW = 15
+LEFT_HAND = 16
+
+KEYPOINTS_NAMELIST = ['BOTTOM_TORSO', 'LEFT_HIP', 'LEFT_KNEE', 'LEFT_FOOT', \
+                      'RIGHT_HIP', 'RIGHT_KNEE', 'RIGHT_FOOT', \
+                      'CENTER_TORSO', 'UPPER_TORS0', 'NOSE', 'CENTER_HEAD', \
+                      'RIGHT_SHOULDER', 'RIGHT_ELBOW', 'RIGHT_HAND', \
+                      'LEFT_SHOULDER', 'LEFT_ELBOW', 'LEFT_HAND']
+
+class Keypoints:
+    """
+    This module has the functionalities as follows:
+    1. Obtain the keypoint of low back
+    2. Obtain the keypoints of shoulders
+    3. Obtain the keypoints of hands
+    """
+    def __init__(self, coordinates, individual=1):
+        self.coordinates = coordinates
+        self.individual = individual
+
+    def low_back_keypoint(self):
+        count = 1
+        low_back_dict = {}
+        for coordinate in self.coordinates:
+            low_back = coordinate[BOTTOM_TORSO].tolist()
+            low_back_dict[f'BOTTOM_TORSO_{count}'] = low_back
+            count += 1
+        low_back_dict_json = Keypoints.to_json(low_back_dict)
+
+        return low_back_dict_json
+
+    def left_shoulder_keypoint(self):
+        count = 1
+        left_shoulder_dict = {}
+        for coordinate in self.coordinates:
+            left_shoulder = coordinate[LEFT_SHOULDER].tolist()
+            left_shoulder_dict[f'LEFT_SHOULDER_{count}'] = left_shoulder
+            count += 1
+        left_shoulder_dict_json = Keypoints.to_json(left_shoulder_dict)
+
+        return left_shoulder_dict_json
+
+    def right_shoulder_keypoint(self):
+        count = 1
+        right_shoulder_dict = {}
+        for coordinate in self.coordinates:
+            right_shoulder = coordinate[RIGHT_SHOULDER].tolist()
+            right_shoulder_dict[f'RIGHT_SHOULDER_{count}'] = right_shoulder
+            count += 1
+        right_shoulder_dict_json = Keypoints.to_json(right_shoulder_dict)
+
+        return right_shoulder_dict_json
+
+    def left_hand_keypoint(self):
+        count = 1
+        left_hand_dict = {}
+        for coordinate in self.coordinates:
+            left_hand = coordinate[LEFT_HAND].tolist()
+            left_hand_dict[f'LEFT_HAND_{count}'] = left_hand
+            count += 1
+        left_hand_dict_json = Keypoints.to_json(left_hand_dict)
+
+        return left_hand_dict_json
+
+    def right_hand_keypoint(self):
+        count = 1
+        right_hand_dict = {}
+        for coordinate in self.coordinates:
+            right_hand = coordinate[LEFT_HAND].tolist()
+            right_hand_dict[f'RIGHT_HAND_{count}'] = right_hand
+            count += 1
+        right_hand_dict_json = Keypoints.to_json(right_hand_dict)
+
+        return right_hand_dict_json
+
+    def to_json(data):
+        return json.dumps(data, indent=4)
